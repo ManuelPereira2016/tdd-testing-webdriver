52e8e0f8b3e7b2260ac48d717a4412714acd29b6
manuel <manuelpereiralds@gmail.com>
2017-09-27 20:42:06 -0400

    Seventh Commit

--- test/wdio.conf.js
+++ test/wdio.conf.js
@@ -114,7 +114,15 @@ exports.config = {
     // Test reporter for stdout.
     // The following are supported: dot (default), spec and xunit
     // see also: http://webdriver.io/guide/testrunner/reporters.html
-    reporter: 'dot',
+    reporters: ['dot', 'junit'],
+    reporterOptions: {
+        junit: {
+            outputDir: './test/reports/',
+            outputFileFormat: function(opts) { // optional
+                return `results-${opts.cid}.${opts.capabilities}.xml`
+            }
+        }
+    },
     //
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
