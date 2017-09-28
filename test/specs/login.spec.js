d22b12eb7d36f839b160fcf99334d8786f804d01
manuel <manuelpereiralds@gmail.com>
2017-09-27 20:55:48 -0400

    Eight commit

--- test/specs/login.spec.js
+++ test/specs/login.spec.js
@@ -2,13 +2,18 @@ describe('login form', function() {
 
     it('should allow access with admin default creds', function*() {
         yield browser.url('/');
+
+        // We want to click "login" on nav so the DOM change its content.
         yield browser.click('#login-btn');
+
+        // Check if we are on Login, wait some seconds.
         yield browser.waitForExist('#login', 10000);
 
         yield browser.isExisting('#login').then(function(isExisting) {
             expect(isExisting).toBe(true);
         });
-        
+
+        // Set the required values on the Form. Test "ADMIN" default user.
         yield browser.setValue('#email', 'admin@123.com');
         yield browser.setValue('#password', '12345');
         yield browser.submitForm('#login');
@@ -19,6 +24,7 @@ describe('login form', function() {
             expect(isExisting).toBe(true);
         });
 
+        // We check if we are on main page already logged in.
         yield browser.getText('#main-text').then(function(text) {
             expect(text).toContain('You have successfully logged in!.');
         });
--- test/specs/register.spec.js
+++ test/specs/register.spec.js
@@ -1,17 +1,27 @@
 describe('register form', function() {
     it('should redirect user to login page', function*() {
         yield browser.url('/');
+
+        // Set name field value on the form.
         yield browser.setValue('#name', 'Manuel Pereira');
+
+        // Set email field value on the form.
         yield browser.setValue('#email', 'manuelpereiralds@gmail.com');
+
+        // Now set the password and confirm it.
         yield browser.setValue('#password', '12345');
         yield browser.setValue('#password-confirm', '12345');
+
+        // Now submit the form and trigger "submit" event.
         yield browser.submitForm('#form');
 
+        // I think 10000 is enough
         yield browser.waitForExist('#login', 10000);
         yield browser.isExisting('#login').then(function(isExisting) {
             expect(isExisting).toBe(true);
         });
 
+        // Lets check if we are now on Login page!
         yield browser.getText('#panel-heading').then(function(msg) {
             expect(msg).toContain('Login');
         });
